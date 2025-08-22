f5547f2d04322ffec33a75f625b2851c8e50c156
Iuliu-Antoniu Popovici <iuliu-antoniu.popovici@analog.com>
2025-08-22 15:45:00 +0300

    ci: Create workflow dispatch and add CI/** trigger check
    Signed-off-by: Iuliu-Antoniu Popovici <iuliu-antoniu.popovici@analog.com>

--- CI/trigger-workflow.sh
+++ CI/trigger-workflow.sh
@@ -19,23 +19,27 @@ post=$(curl -s -i -L -X POST \
         }')
 
 if [[ ! -z $(echo "$post" | grep "HTTP/2 20*" ) ]]; then
-    echo "Post request to trigger private workflow success!"
+    echo -e "\nPost request to trigger private workflow successful!"
 
     # Search for the triggered build using the opensource sent run id
     run_search=""
     while [[ $(echo "$run_search" | grep ^'      "name":' | \
                 cut -d "-" -f2 | sed 's#"##g' | sed 's#,##g' | \
                 sed 's# ##g' | awk 'NR==1{print $NF}') != ${TRIGGER_ID} ]]; do
+
+        echo -e "\nCatching the triggered build ..."
+
         run_search=$(curl -s -L \
                     -H "Accept: application/vnd.github+json" \
                     -H "Authorization: Bearer ${GH_TRIGGER_TOKEN}" \
                     -H "X-GitHub-Api-Version: 2022-11-28" \
                     https://api.github.com/repos/adi-innersource/hdl-builds/actions/workflows/${PRIVATE_WORKFLOW}/runs)
     done
-    echo "Catched the triggered run build! Checking the status of m2k-fw build ..."
 
     # Check status of the run by getting private run id
     run_id=$(echo "$run_search" | grep ^'      "id":' | awk 'NR==1{print $NF}' | cut -d ":" -f2 | sed 's#,##g')
+    echo -e "\nCatched the triggered build! Build m2k-fw run id: [ ${run_id} ]"
+    echo -e "\nChecking status of m2k-fw build ..."
 
     run_status=""
     while [[ "$run_status" != "completed" ]]; do
@@ -46,32 +50,24 @@ if [[ ! -z $(echo "$post" | grep "HTTP/2 20*" ) ]]; then
                     -H "X-GitHub-Api-Version: 2022-11-28" \
                     https://api.github.com/repos/adi-innersource/hdl-builds/actions/runs/${run_id} | \
                     grep ^'  "status":' | cut -d ":" -f2 | sed 's#"##g' | sed 's#,##g' | sed 's# ##g')
+
         echo "Status of m2k-fw build: ${run_status} ..."
-
-        # Check if it was cancelled
-        conclusion=$(echo "$run_status"| grep ^'      "conclusion":' | \
-                        awk 'NR==1{print $NF}' | cut -d ":" -f2 | sed 's#,##g')
-        if [ "$conclusion" == "cancelled" ]; then
-            echo "Build was cancelled ... exiting!"
-            exit 1
-        fi
-
     done
-    echo "Build finished! Getting result ..."
+    echo -e "\nBuild finished! Getting result ..."
 
     # Get build result conclusion
     conclusion=$(echo "$run_status"| grep ^'      "conclusion":' | \
                     awk 'NR==1{print $NF}' | cut -d ":" -f2 | sed 's#,##g')
 
     if [ "$conclusion" != "success" ]; then
-        echo "Build failed!"
+        echo -e "\nBuild conclusion: ${conclusion}!"
         exit 1
     else
-        echo "Build finished successfuly!"
+        echo -e "\nBuild finished successfuly!"
     fi
 
 else
-    echo "Post request to trigger private workflow failed ..."
+    echo "\nPost request to trigger private workflow failed ..."
     echo "$post"
     exit 1
 fi
