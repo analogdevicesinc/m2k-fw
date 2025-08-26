e682860f76c52b6334dc97dcb93fbecd6cb71dad
Iuliu-Antoniu Popovici <iuliu-antoniu.popovici@analog.com>
2025-08-26 23:39:37 +0300

    Add check cleanup fixes
    Signed-off-by: Iuliu-Antoniu Popovici <iuliu-antoniu.popovici@analog.com>

--- CI/trigger-workflow.sh
+++ CI/trigger-workflow.sh
@@ -33,7 +33,7 @@ if [[ ! -z $(echo "$post" | grep "HTTP/2 20*" ) ]]; then
                     -H "Accept: application/vnd.github+json" \
                     -H "Authorization: Bearer ${GH_TRIGGER_TOKEN}" \
                     -H "X-GitHub-Api-Version: 2022-11-28" \
-                    https://api.github.com/repos/adi-innersource/hdl-builds/actions/workflows/${PRIVATE_WORKFLOW}/runs)
+                    https://api.github.com/repos/${PRIVATE_ORG}/${PRIVATE_REPO}/actions/workflows/${PRIVATE_WORKFLOW}/runs)
     done
 
     # Check status of the run by getting private run id
@@ -48,8 +48,8 @@ if [[ ! -z $(echo "$post" | grep "HTTP/2 20*" ) ]]; then
                         -H "Accept: application/vnd.github+json" \
                         -H "Authorization: Bearer ${GH_TRIGGER_TOKEN}" \
                         -H "X-GitHub-Api-Version: 2022-11-28" \
-                        https://api.github.com/repos/adi-innersource/hdl-builds/actions/runs/${run_id})
-        run_status=$(echo "$status_request" | grep ^'  "status":' | cut -d ":" -f2 |\
+                        https://api.github.com/repos/${PRIVATE_ORG}/${PRIVATE_REPO}/actions/runs/${run_id})
+        run_status=$(echo "$status_request" | grep ^'  "status":' | cut -d ":" -f2 | \
                      sed 's#"##g' | sed 's#,##g' | sed 's# ##g')
 
         echo "Status of m2k-fw build: ${run_status} ..."
@@ -57,8 +57,8 @@ if [[ ! -z $(echo "$post" | grep "HTTP/2 20*" ) ]]; then
     echo -e "\nBuild finished! Getting result ..."
 
     # Get build result conclusion
-    conclusion=$(echo "$status_request" | grep ^'      "conclusion":' | \
-                    awk 'NR==1{print $NF}' | cut -d ":" -f2 | sed 's#,##g')
+    conclusion=$(echo "$status_request" | grep ^'  "conclusion":' | cut -d ":" -f2 | \
+                    sed 's#"##g' | sed 's#,##g' | sed 's# ##g')
 
     if [ "$conclusion" != "success" ]; then
         echo -e "\nBuild conclusion: ${conclusion}!"
